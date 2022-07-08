2c7a68d9c6b89a5d49c1bbacc23f892ed3cbd16c
Ilya Beda <ir4y.ix@gmail.com>
2022-07-08 21:51:58 +0400

    Initial Commit

---
+++ TestState/TestStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestStateApp.swift
+//  TestState
+//
+//  Created by Ilya Beda on 08.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
