9819809b49f02fd57e06427f390fb885a5d0e733
Ilya Beda <ir4y.ix@gmail.com>
2022-07-11 11:57:58 +0400

    State managment example

--- TestState/ContentView.swift
+++ TestState/ContentView.swift
@@ -7,10 +7,98 @@
 
 import SwiftUI
 
+typealias State = [String: Value]
+
+enum Value {
+    case string(String)
+    case number(Int32)
+    case state(State)
+}
+
+func formatValue(v: Value) -> String {
+    switch v {
+    case .number(let n):
+        return String(n)
+    case .string(let s):
+        return s
+    case .state(let state):
+        var result = "{"
+        state.forEach { (key, value) in
+            result = result + key + ":" + formatValue(v: value) + ","
+        }
+        return result + "}";
+    }
+}
+
+class DictModel: ObservableObject {
+    @Published var dict: State = [:]
+    
+    func bind(path:[String]) -> Binding<String>{
+        return Binding<String>(get: {
+            let val = getByPath(state: self.dict, path: path);
+                switch val {
+                    case .string(let s):
+                    return s;
+                default:
+                    return "";
+                }
+        }, set: {
+            setByPath(state: &self.dict, path:path, newValue: Value.string($0))
+                
+        })
+    }
+}
+
+func getByPath(state: State, path:[String]) -> Value? {
+    if(path.count == 0){
+        return nil
+    }
+    if(path.count == 1){
+        return state[path.first!]
+    }
+    let value = state[path.first!]
+    switch value {
+        case .state(let state):
+            return getByPath(state: state, path: Array(path.dropFirst()))
+        default:
+            return nil
+    }
+}
+
+ func setByPath(state: inout State, path:[String], newValue: Value) {
+    if(path.count == 0){
+        return
+    }
+    if(path.count == 1){
+        state[path.first!] = newValue
+        return
+    }
+    let value = state[path.first!]
+    switch value {
+        case .state(var state):
+            setByPath(state: &state,
+                      path: Array(path.dropFirst()),
+                      newValue: newValue)
+        default:
+            return
+    }
+}
+
+
 struct ContentView: View {
+    @ObservedObject var vm = DictModel()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("First name",
+                      text: vm.bind(path: ["firstName"]))
+            TextField("Last name",
+                      text: vm.bind(path: ["lastName"]))
+            Divider()
+            Button("Reset") { self.vm.dict = [:] }
+            Text(formatValue(v: Value.state(self.vm.dict)))
+        }
+
     }
 }
 
